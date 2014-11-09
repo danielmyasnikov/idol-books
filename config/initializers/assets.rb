2ebab260d846626effe84a70b8b916dfd31b9d9d
Daniel Myasnikov <daniel.myasnikov@redant.com.au>
2014-11-09 20:36:17 +1100

    working profile mockup

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,3 +7,33 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( app.css )
 Rails.application.config.assets.precompile += %w( app.js )
+
+Dir.entries(Rails.root.join("app", 'assets', 'stylesheets').to_s).each do |file|
+  Rails.application.config.assets.precompile += [file]  
+end
+
+Dir.entries(Rails.root.join("app", 'assets', 'javascripts').to_s).each do |file|
+  Rails.application.config.assets.precompile += [file]
+end
+
+Dir.entries(Rails.root.join("app", 'assets', 'fonts').to_s).each do |file|
+  Rails.application.config.assets.precompile += [file]
+end
+
+Dir.entries(Rails.root.join("app", 'assets', 'images').to_s).each do |file|
+  Rails.application.config.assets.precompile += [file]
+end
+
+Dir.entries(Rails.root.join("app", 'assets', 'images', 'people', '110').to_s).each do |file|
+  Rails.application.config.assets.precompile += [file]
+end
+
+Dir.entries(Rails.root.join("app", 'assets', 'images', 'people', '50').to_s).each do |file|
+  Rails.application.config.assets.precompile += [file]
+end
+
+Dir.entries(Rails.root.join("app", 'assets', 'images', 'social', '100').to_s).each do |file|
+  Rails.application.config.assets.precompile += [file]
+end
+#  
+
